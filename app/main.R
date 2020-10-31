241f197fc69000ddb46ae98d0974a91675f47ad0
“Kota <tozawakota@gmail.com>
2020-10-31 18:23:03 +0900

    add main.R

---
+++ app/main.R
@@ -0,0 +1,18 @@
+library(jsonlite)
+library(dplyr)
+library(rtweet)
+library(ggplot2)
+library(tidytext)
+
+# Twitter APIの認証情報読み込み
+auth <- "auth.json" %>%
+  jsonlite::read_json(simplifyVector = TRUE)
+
+# Twitter APIアクセス用トークン取得
+token <- create_token(
+  app = auth$MY_APP,
+  consumer_key = auth$CONSUMER_KEY,
+  consumer_secret = auth$CONSUMER_SECRET,
+  access_token = auth$ACCESS_TOKEN,
+  access_secret = auth$ACCESS_SECRET
+)

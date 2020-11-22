e1188ecef6d013c5bf6de97252be06cc5a71e2c0
“Kota <tozawakota@gmail.com>
2020-11-23 01:32:31 +0900

    fix typo.

--- ingestion/visualize/visualize-tweet-freq.R
+++ ingestion/visualize/visualize-tweet-freq.R
@@ -6,7 +6,7 @@ library(ggplot2)
 #' ツイート頻度の時系列データ取得
 #'
 #' @param user character Twitterユーザー名（先頭にアットマークは付けない）
-#' \code{visualize_tweet_freq_time_series} download_user_tweets()で得たツイートデータから、Rechartで可視化するために必要な値を取り出して加工し、リストに詰めて返す
+#' \code{visualize_tweet_freq_time_series} download_user_tweets()で得たツイートデータから、Rechartsで可視化するために必要な値を取り出して加工し、リストに詰めて返す
 #' breaks: 年月
 #' freqs: ツイート頻度
 #' mertics: ツイート数合計や平均などの各種統計量・メトリクス（下記の5つ）が入ったリスト
@@ -47,7 +47,7 @@ visualize_tweet_freq_time_series <- function(user) {
     full_join(tweets_per_day, by = "CREATED_AT") %>%
     replace_na(list(FREQ = 0))
 
-  # Rechartで作る時系列グラフ用データを用意
+  # Rechartsで作る時系列グラフ用データを用意
   # TODO 移動平均の時系列グラフ用データも用意する（7日間周期でやってみる？）
   breaks <- all_dates
   freqs <- tweets_per_day_imputed$FREQ
@@ -55,5 +55,5 @@ visualize_tweet_freq_time_series <- function(user) {
   return(list(breaks, freqs))
 }
 
-# TODO 度数分布表のヒストグラムをRechartで出すためのデータを用意する関数を作る
+# TODO 度数分布表のヒストグラムをRechartsで出すためのデータを用意する関数を作る
 # visualize_tweet_freq_histogram <- function(user) {}

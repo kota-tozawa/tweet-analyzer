2bbdb1936dc7d748cbe596cd595f14d7fffeb730
“Kota <tozawakota@gmail.com>
2020-11-26 01:43:42 +0900

    refactor.

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,6 +4,7 @@ test_check("tweet-analyzer")
 
 # テストデータ読み込み（2020/11/22に3200件取得した厚生労働省のツイートデータ）
 tws <- load("../../output/raw/rdata/_testdata.Rdata")
+# 前処理
 tws$CREATED_AT <- as.character(tws$CREATED_AT) %>%
   substr(1, 10) %>%
   ymd()

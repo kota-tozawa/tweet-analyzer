faf54739416e80dc45eb20807c8d2ed2274496df
“Kota <tozawakota@gmail.com>
2020-11-29 02:50:54 +0900

    update react app.

--- src/_app.jsx
+++ src/_app.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import clsx from 'clsx';
 import {
   Button,
   TextField,
@@ -9,28 +9,106 @@ import {
   makeStyles,
   withStyles,
   FormControl,
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Drawer,
+  Divider,
+  List,
+  Grid,
+  Paper,
 } from '@material-ui/core';
-import { SendRounded } from '@material-ui/icons';
-import { Options } from './common/constants';
+import { mainListItems } from './components/parts/listItems';
+import { SendRounded, Menu, ChevronLeft } from '@material-ui/icons';
+import { Options } from './components/parts/constants';
 import LineGraph from './components/tweetFrequency/LineGraph';
 
-const useStyles = makeStyles({
-  interval: {
-    marginTop: '16px',
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
   },
   paper: {
-    padding: '24px',
-  },
-  buttonWrapper: {
+    padding: theme.spacing(2),
     display: 'flex',
-    justifyContent: 'flex-end',
+    overflow: 'auto',
+    flexDirection: 'column',
   },
-  errorMessage: {
-    fontSize: '0.8rem',
-    color: '#F44036',
-    marginTop: '8px',
+  fixedHeight: {
+    height: 240,
   },
-});
+}));
 
 // TODO 関数コンポーネントに書き換える
 class App extends Component {
@@ -40,9 +118,12 @@ class App extends Component {
       user: '',
       ntweets: 400,
       lineGraphData: null,
+      open: true,
     };
     this.handleUserChange = this.handleUserChange.bind(this);
     this.handleNtweetsChange = this.handleNtweetsChange.bind(this);
+    this.handleDrawerOpen = this.handleDrawerOpen.bind(this);
+    this.handleDrawerClose = this.handleDrawerClose.bind(this);
   }
 
   componentDidMount() {
@@ -64,62 +145,143 @@ class App extends Component {
     window.Shiny.onInputChange('ntweets', this.state.ntweets);
   }
 
-  handleUserChange(event) {
-    const value = event.target.value;
+  handleUserChange(e) {
+    const value = e.target.value;
     this.setState({ user: value });
   }
 
-  handleNtweetsChange(event) {
-    const value = parseInt(event.target.value);
+  handleNtweetsChange(e) {
+    const value = parseInt(e.target.value);
     this.setState({ ntweets: value });
+  }
+
+  handleDrawerOpen() {
+    this.setState({ open: true });
+  }
+
+  handleDrawerClose() {
+    this.setState({ open: false });
   }
 
   render() {
     const { classes } = this.props;
+    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
     const { user, ntweets, lineGraphData } = this.state;
     return (
-      <Container maxWidth="lg">
-        {/* <Paper className={classes.paper} elevation={3}> */}
-        <h3 className="mt-3">パラメーター入力</h3>
-        <FormControl margin="normal">
-          <TextField
-            required
-            id="user"
-            value={user}
-            placeholder="Twitterのユーザー名"
-            label="Twitterのユーザー名"
-            variant="outlined"
-            fullWidth
-            onChange={this.handleUserChange}
-          />
-          <Select
-            required
-            id="ntweets"
-            value={ntweets}
-            onChange={this.handleNtweetsChange}
-            fullWidth
-          >
-            {Options.map((option) => (
-              <MenuItem key={option} value={option}>
-                {option}
-              </MenuItem>
-            ))}
-          </Select>
-          <Button
-            type="submit"
-            className={classes.interval}
-            variant="contained"
-            color="primary"
-            endIcon={<SendRounded />}
-          >
-            Send
-          </Button>
-        </FormControl>
-        {lineGraphData && <LineGraph {...lineGraphData} xAxisLabel="年月" />}
-        {/* </Paper> */}
-      </Container>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="absolute"
+          className={clsx(
+            classes.appBar,
+            this.state.open && classes.appBarShift
+          )}
+        >
+          <Toolbar className={classes.toolbar}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={this.handleDrawerOpen}
+              className={clsx(
+                classes.menuButton,
+                this.state.open && classes.menuButtonHidden
+              )}
+            >
+              <Menu />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              className={classes.title}
+            >
+              ツイート分析用ダッシュボード
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="persistent"
+          classes={{
+            paper: clsx(
+              classes.drawerPaper,
+              !this.state.open && classes.drawerPaperClose
+            ),
+          }}
+          open={this.state.open}
+        >
+          <div className={classes.toolbarIcon}>
+            <IconButton onClick={this.handleDrawerClose}>
+              <ChevronLeft />
+            </IconButton>
+          </div>
+          <Divider />
+          <List>{mainListItems}</List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.appBarSpacer} />
+          <Container maxWidth="lg" className={classes.container}>
+            <Grid container spacing={3}>
+              {/* 分析パラメータ入力フォーム */}
+              <Grid item xs={12} md={4} lg={3}>
+                <Paper className={fixedHeightPaper}>
+                  <FormControl margin="normal">
+                    <TextField
+                      required
+                      id="user"
+                      value={user}
+                      placeholder="Twitterのユーザー名"
+                      label="Twitterのユーザー名"
+                      variant="outlined"
+                      fullWidth
+                      onChange={this.handleUserChange}
+                    />
+                    <Select
+                      required
+                      id="ntweets"
+                      value={ntweets}
+                      onChange={this.handleNtweetsChange}
+                      fullWidth
+                    >
+                      {Options.map((option) => (
+                        <MenuItem key={option} value={option}>
+                          {option}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                    <Button
+                      type="submit"
+                      className={classes.interval}
+                      variant="contained"
+                      color="primary"
+                      endIcon={<SendRounded />}
+                    >
+                      Send
+                    </Button>
+                  </FormControl>
+                </Paper>
+              </Grid>
+              {/* ツイート頻度の度数分布表
+              <Grid item xs={12} md={8} lg={9}>
+                <Paper className={fixedHeightPaper}>
+                  <Deposits />
+                </Paper>
+              </Grid> */}
+              {/* ツイート頻度の時系列グラフ */}
+              <Grid item xs={12}>
+                <Paper className={classes.paper}>
+                  {lineGraphData && (
+                    <LineGraph {...lineGraphData} xAxisLabel="年月" />
+                  )}
+                </Paper>
+              </Grid>
+            </Grid>
+          </Container>
+        </main>
+      </div>
     );
   }
 }
 
-export default withStyles(useStyles)(App);
+export default withStyles(useStyles, { withTheme: true })(App);

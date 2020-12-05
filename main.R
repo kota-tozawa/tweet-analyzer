e2128c33e47e4ccda99e1e4256dedf0dd40b7c56
“Kota <tozawakota@gmail.com>
2020-12-05 20:03:32 +0900

    add an option for debug.

--- main.R
+++ main.R
@@ -5,5 +5,7 @@ options(shiny.launch.browser = FALSE)
 # サーバーサイドの変更でリロード
 options(shiny.autoreload = TRUE)
 options(shiny.autoreload.pattern = ".*\\.(r|html)$")
+# R（Shiny）とJavaScript（React）の間のやり取り（WebSocket）をコンソールに出力
+options(shiny.trace = TRUE)
 
 shiny::runApp()

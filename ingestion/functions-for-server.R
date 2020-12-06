bcdcfc802e5ad1fc95559c86253a4a4b9a13a54e
“Kota <tozawakota@gmail.com>
2020-12-06 13:16:12 +0900

    update did_download_with_same_info function.

--- ingestion/functions-for-server.R
+++ ingestion/functions-for-server.R
@@ -1,8 +1,8 @@
 # 過去に同じユーザー名・ツイート数で既にAPIからデータを取得しているかどうかを判定する関数
 did_download_with_same_info <- function(user, ntweets) {
   all_filenames <- list.files("output/raw/rdata/", pattern = "Rdata")
+  # 拡張子を除く
   all_filenames <- sub("\\.[^.]*", "", all_filenames)
-  all_filenames <- sub("_testdata", "", all_filenames)
 
   # 想定されるファイル名（拡張子 .Rdata 抜き）
   assumed_file_name <- paste0(user, "-", ntweets)

1e201700fbb4f6245bafd8db9592ef6210477f7f
“Kota <tozawakota@gmail.com>
2020-12-08 20:33:24 +0900

    refactor.

--- src/components/pages/SentimentAnalysis.jsx
+++ src/components/pages/SentimentAnalysis.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Typography, Link } from '@material-ui/core';
 
-const WordCloud = () => {
+const SentimentAnalysis = () => {
   return (
     <>
       <Typography paragraph>ツイート内容のセンチメント分析</Typography>
@@ -17,4 +17,4 @@ const WordCloud = () => {
   );
 };
 
-export default WordCloud;
+export default SentimentAnalysis;

d7c4614f21bdde273ed885bcd63ee951e2e58d7a
“Kota <tozawakota@gmail.com>
2020-12-20 10:19:02 +0900

    add word frequency table.

---
+++ src/components/organisms/WordCloudViz/WordFreqTable.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@material-ui/core';
+
+const WordFreqTable = ({ words, freqs }) => {
+  const rows = freqs
+    .map((freq, i) => ({
+      word: words[i],
+      freq: freq,
+    }))
+    .slice(0, 20);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">単語</TableCell>
+            <TableCell align="center">出現頻度</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, i) => (
+            <TableRow key={i}>
+              <TableCell key={row.word} align="center">
+                {row.word}
+              </TableCell>
+              <TableCell key={row.freq} align="center">
+                {row.freq}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+WordFreqTable.propTypes = {
+  words: PropTypes.arrayOf(PropTypes.string).isRequired,
+  freqs: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
+
+export default WordFreqTable;
--- src/components/pages/WordCloud.jsx
+++ src/components/pages/WordCloud.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-import { Typography, Link } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { Grid, Typography, Link, withStyles, Paper } from '@material-ui/core';
 import * as Consts from '../atoms/constants';
 import BaseForm from '../organisms/Forms/BaseForm';
 import WordCloudViz from '../organisms/WordCloudViz/WordCloudViz';
+import WordFreqTable from '../organisms/WordCloudViz/WordFreqTable';
+
+const styles = (theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
 
 // TODO 関数コンポーネントに書き換える。現状Hooksを用いたWebSocketによるRとJavaScript間の通信を上手く扱えずできていない。
 class WordCloud extends Component {
@@ -24,37 +37,59 @@ class WordCloud extends Component {
 
   render() {
     const { dataIngested, analysisType } = this.state;
+    const { classes } = this.props;
     return (
-      <>
-        <h2>ツイート内容のワードクラウド</h2>
-        <Typography paragraph>
-          <Link
-            href="https://ja.wikipedia.org/wiki/%E3%82%BF%E3%82%B0%E3%82%AF%E3%83%A9%E3%82%A6%E3%83%89"
-            target="_blank"
-          >
-            ワードクラウドとは？
-          </Link>
-        </Typography>
-        <BaseForm
-          analysisType={analysisType}
-          options={Consts.ntweetOptionsLong}
-        />
-        <h3>{dataIngested && dataIngested['title']}</h3>
-        <Typography component={'span'}>
-          <pre>
-            {dataIngested &&
-              '注意：「取得するツイート数」が小さいとワードクラウドがきれいに描画されない場合があります。'}
-          </pre>
-        </Typography>
-        {dataIngested && (
-          <WordCloudViz
-            words={dataIngested['words']}
-            freqs={dataIngested['freqs']}
-          />
-        )}
-      </>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <h2>ツイート内容のワードクラウド</h2>
+            <Typography paragraph>
+              <Link
+                href="https://ja.wikipedia.org/wiki/%E3%82%BF%E3%82%B0%E3%82%AF%E3%83%A9%E3%82%A6%E3%83%89"
+                target="_blank"
+              >
+                ワードクラウドとは？
+              </Link>
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <BaseForm
+              analysisType={analysisType}
+              options={Consts.ntweetOptionsLong}
+            />
+          </Grid>
+          <Grid item xs={9}>
+            <h3>{dataIngested && dataIngested['title']}</h3>
+            <Typography component={'span'}>
+              <pre>
+                {dataIngested &&
+                  '注意：「取得するツイート数」が小さいとワードクラウドがきれいに描画されない場合があります。'}
+              </pre>
+            </Typography>
+            {dataIngested && (
+              <WordCloudViz
+                words={dataIngested['words']}
+                freqs={dataIngested['freqs']}
+              />
+            )}
+          </Grid>
+          <Grid item xs={3}>
+            <h3>{dataIngested && '出現頻度の多い単語 トップ20'}</h3>
+            {dataIngested && (
+              <WordFreqTable
+                words={dataIngested['words']}
+                freqs={dataIngested['freqs']}
+              />
+            )}
+          </Grid>
+        </Grid>
+      </div>
     );
   }
 }
 
-export default WordCloud;
+WordCloud.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(WordCloud);
